7c59b5360cb617ad5ac99c38ca519d72ef245423
mz01-junghunee <cjswovkdnj12@naver.com>
2025-02-28 15:59:49 +0900

    feat : 목표없는 노트 목록 필터링 요소 추가

--- src/main/java/com/todo/mirujima_be/note/dto/request/NoteListRequest.java
+++ src/main/java/com/todo/mirujima_be/note/dto/request/NoteListRequest.java
@@ -14,13 +14,15 @@ import lombok.Setter;
 @AllArgsConstructor
 public class NoteListRequest {
 
-    @Schema(description = "목표 고유키", example = "1")
-    private Long goalId;
-    @Schema(description = "현재 페이지에서 가장 작은 노트 고유키", example = "9999", defaultValue = "9223372036854775807")
-    private Long lastSeenId = Long.MAX_VALUE;
-    @Min(1)
-    @Max(100)
-    @Schema(description = "페이지 크기", example = "5", defaultValue = "5")
-    private Integer pageSize = 5;
+  @Schema(description = "목표 고유키", example = "1")
+  private Long goalId;
+  @Schema(description = "현재 페이지에서 가장 작은 노트 고유키", example = "9999", defaultValue = "9223372036854775807")
+  private Long lastSeenId = Long.MAX_VALUE;
+  @Min(1)
+  @Max(100)
+  @Schema(description = "페이지 크기", example = "5", defaultValue = "5")
+  private Integer pageSize = 5;
+  @Schema(description = "목표 보유 유무", example = "true")
+  private Boolean hasGoal;
 
 }
--- src/main/java/com/todo/mirujima_be/note/repository/CustomNoteRepositoryImpl.java
+++ src/main/java/com/todo/mirujima_be/note/repository/CustomNoteRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.todo.mirujima_be.note.repository;
 
+import static com.todo.mirujima_be.goal.entity.QGoal.goal;
 import static com.todo.mirujima_be.note.entity.QNote.note;
 import static com.todo.mirujima_be.todo.entity.QTodo.todo;
 import static com.todo.mirujima_be.user.entity.QUser.user;
@@ -21,15 +22,17 @@ public class CustomNoteRepositoryImpl implements CustomNoteRepository {
   public List<Long> getNoteIdList(NoteListRequest noteListRequest) {
     var goalId = noteListRequest.getGoalId();
     var lastSeenId = noteListRequest.getLastSeenId();
+    var hasGoal = noteListRequest.getHasGoal();
     var pageSize = noteListRequest.getPageSize();
     if (pageSize == null) {
       pageSize = MirujimaConstants.Note.PAGE_SIZE;
     }
     // 쿼리 조건 생성
-    var condition = getCondition(goalId, lastSeenId);
+    var condition = getCondition(goalId, lastSeenId, hasGoal);
     return factory.select(note.id)
         .from(note)
         .join(note.todo, todo)
+        .join(todo.goal, goal)
         .join(note.createdBy, user)
         .where(condition)
         .orderBy(note.id.desc())
@@ -41,17 +44,19 @@ public class CustomNoteRepositoryImpl implements CustomNoteRepository {
   public Long getNoteCount(NoteListRequest noteListRequest) {
     var goalId = noteListRequest.getGoalId();
     var lastSeenId = noteListRequest.getLastSeenId();
+    var hasGoal = noteListRequest.getHasGoal();
     // 쿼리 조건 생성
-    var condition = getCondition(goalId, lastSeenId);
+    var condition = getCondition(goalId, lastSeenId, hasGoal);
     return factory.select(note.count())
         .from(note)
         .join(note.todo, todo)
+        .join(todo.goal, goal)
         .join(note.createdBy, user)
         .where(condition)
         .fetchOne();
   }
 
-  private BooleanBuilder getCondition(Long goalId, Long lastSeenId) {
+  private BooleanBuilder getCondition(Long goalId, Long lastSeenId, Boolean hasGoal) {
     var condition = new BooleanBuilder();
     condition.and(note.createdBy.email.eq(AuthUtil.getEmail()));
     if (goalId != null) {
@@ -59,6 +64,13 @@ public class CustomNoteRepositoryImpl implements CustomNoteRepository {
     }
     if (lastSeenId != null) {
       condition.and(note.id.lt(lastSeenId));
+    }
+    if (hasGoal != null) {
+      if (hasGoal) {
+        condition.and(goal.id.isNotNull());
+      } else {
+        condition.and(goal.id.isNull());
+      }
     }
     return condition;
   }
