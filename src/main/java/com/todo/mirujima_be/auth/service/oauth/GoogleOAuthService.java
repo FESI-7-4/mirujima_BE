6c98cf838dd313e43cf5ba1fa9251f03fd7e1edc
junghunee <cjswovkdnj12@naver.com>
2025-03-09 17:13:18 +0900

    fix : OAuth 인증 에러 픽스

--- src/main/java/com/todo/mirujima_be/auth/service/oauth/GoogleOAuthService.java
+++ src/main/java/com/todo/mirujima_be/auth/service/oauth/GoogleOAuthService.java
@@ -15,54 +15,55 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GoogleOAuthService implements OAuthService {
 
-  private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
 
-  @Value("${google.tokenUrl}")
-  private String tokenUrl;
-  @Value("${google.userInfoUrl}")
-  private String userInfoUrl;
-  @Value("${google.client.id}")
-  private String clientId;
-  @Value("${google.client.secret}")
-  private String clientSecret;
-  @Value("${google.redirect.uri}")
-  private String redirectUri;
+    @Value("${google.tokenUrl}")
+    private String tokenUrl;
+    @Value("${google.userInfoUrl}")
+    private String userInfoUrl;
+    @Value("${google.client.id}")
+    private String clientId;
+    @Value("${google.client.secret}")
+    private String clientSecret;
+    @Value("${google.redirect.uri}")
+    private String redirectUri;
 
-  @Override
-  public String getAccessToken(String code) {
-    var headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    @Override
+    public String getAccessToken(String code) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-    var params = new LinkedMultiValueMap<String, String>();
-    params.add("code", code);
-    params.add("client_id", clientId);
-    params.add("client_secret", clientSecret);
-    params.add("redirect_uri", redirectUri);
-    params.add("grant_type", "authorization_code");
+        var params = new LinkedMultiValueMap<String, String>();
+        params.add("code", code);
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUri);
+        params.add("grant_type", "authorization_code");
 
-    var request = new HttpEntity<>(params, headers);
-    var response = restTemplate.postForEntity(tokenUrl, request, JsonNode.class);
-    var responseBody = response.getBody();
+        var request = new HttpEntity<>(params, headers);
+        var response = restTemplate.postForEntity(tokenUrl, request, JsonNode.class);
+        var responseBody = response.getBody();
 
-    if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
-      throw new AlertException("Google OAuth 인증 에러가 발생하였습니다.");
+        if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
+            throw new AlertException("Google OAuth 인증 에러가 발생하였습니다.");
+        }
+        return responseBody.get("access_token").asText();
     }
-    return responseBody.get("access_token").asText();
-  }
 
-  @Override
-  public OAuthUserInfo getUserInfo(String accessToken) {
-    var headers = new HttpHeaders();
-    headers.setBearerAuth(accessToken);
+    @Override
+    public OAuthUserInfo getUserInfo(String accessToken) {
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-    var request = new HttpEntity<>(headers);
-    var response = restTemplate.getForEntity(userInfoUrl, OAuthUserInfo.class, request);
-    var responseBody = response.getBody();
+        var request = new HttpEntity<>(headers);
+        var response = restTemplate.postForEntity(userInfoUrl, request, OAuthUserInfo.class);
+        var responseBody = response.getBody();
 
-    if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
-      throw new AlertException("Google 회원 정보 조회중 에러가 발생하였습니다.");
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new AlertException("Google 회원 정보 조회중 에러가 발생하였습니다.");
+        }
+        return responseBody;
     }
-    return responseBody;
-  }
 }
 
--- src/main/java/com/todo/mirujima_be/auth/service/oauth/KakaoOAuthService.java
+++ src/main/java/com/todo/mirujima_be/auth/service/oauth/KakaoOAuthService.java
@@ -15,58 +15,58 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class KakaoOAuthService implements OAuthService {
 
-  private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
 
-  @Value("${kakao.tokenUrl}")
-  private String tokenUrl;
-  @Value("${kakao.userInfoUrl}")
-  private String userInfoUrl;
-  @Value("${kakao.client.id}")
-  private String clientId;
-  @Value("${kakao.client.secret}")
-  private String clientSecret;
-  @Value("${kakao.redirect.uri}")
-  private String redirectUri;
+    @Value("${kakao.tokenUrl}")
+    private String tokenUrl;
+    @Value("${kakao.userInfoUrl}")
+    private String userInfoUrl;
+    @Value("${kakao.client.id}")
+    private String clientId;
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+    @Value("${kakao.redirect.uri}")
+    private String redirectUri;
 
-  @Override
-  public String getAccessToken(String code) {
-    var headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    @Override
+    public String getAccessToken(String code) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-    var params = new LinkedMultiValueMap<String, String>();
-    params.add("code", code);
-    params.add("client_id", clientId);
-    params.add("client_secret", clientSecret);
-    params.add("redirect_uri", redirectUri);
-    params.add("grant_type", "authorization_code");
+        var params = new LinkedMultiValueMap<String, String>();
+        params.add("code", code);
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUri);
+        params.add("grant_type", "authorization_code");
 
-    var request = new HttpEntity<>(params, headers);
-    var response = restTemplate.postForEntity(tokenUrl, request, JsonNode.class);
-    var responseBody = response.getBody();
+        var request = new HttpEntity<>(params, headers);
+        var response = restTemplate.postForEntity(tokenUrl, request, JsonNode.class);
+        var responseBody = response.getBody();
 
-    if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
-      throw new AlertException("Kakao OAuth 인증 에러가 발생하였습니다.");
-    }
-    return responseBody.get("access_token").asText();
-  }
-
-  @Override
-  public OAuthUserInfo getUserInfo(String accessToken) {
-    var headers = new HttpHeaders();
-    headers.setBearerAuth(accessToken);
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-    var request = new HttpEntity<>(headers);
-    var response = restTemplate.postForEntity(userInfoUrl, request, JsonNode.class);
-    var responseBody = response.getBody();
-
-    if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
-      throw new AlertException("Kakao 회원 정보 조회중 에러가 발생하였습니다.");
+        if (response.getStatusCode() != HttpStatus.OK || responseBody == null) {
+            throw new AlertException("Kakao OAuth 인증 에러가 발생하였습니다.");
+        }
+        return responseBody.get("access_token").asText();
     }
 
-    return OAuthUserInfo.builder()
-        .email(responseBody.get("kakao_account").get("email").asText())
-        .name(responseBody.get("kakao_account").get("name").asText())
-        .build();
-  }
+    @Override
+    public OAuthUserInfo getUserInfo(String accessToken) {
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        var request = new HttpEntity<>(headers);
+        var response = restTemplate.postForEntity(userInfoUrl, request, JsonNode.class);
+        var responseBody = response.getBody();
+
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new AlertException("Kakao 회원 정보 조회중 에러가 발생하였습니다.");
+        }
+
+        return OAuthUserInfo.builder()
+                .email(responseBody.get("kakao_account").get("email").asText())
+                .name(responseBody.get("properties").get("nickname").asText())
+                .build();
+    }
 }
